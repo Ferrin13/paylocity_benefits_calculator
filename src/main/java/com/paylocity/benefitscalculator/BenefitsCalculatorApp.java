0fef580f8774122c2c8a5e2cdf9f150245dd2c7c
ccook <13auction13@gmail.com>
2019-01-08 14:07:06 -0700

    Initial Commit

---
+++ src/main/java/com/paylocity/benefitscalculator/BenefitsCalculatorApp.java
@@ -0,0 +1,11 @@
+package com.paylocity.benefitscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefitsCalculatorApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BenefitsCalculatorApp.class, args);
+    }
+}
