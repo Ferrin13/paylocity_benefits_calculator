c3e6ea78cfd8de4ffd692c3c8ed803fac3b1fd79
ccook <13auction13@gmail.com>
2019-01-10 16:56:54 -0700

    Implemented proof of concept GET endpoint

--- src/main/java/com/paylocity/benefitscalculator/BenefitsCalculatorController.java
+++ src/main/java/com/paylocity/benefitscalculator/BenefitsCalculatorController.java
@@ -1,4 +1,14 @@
 package com.paylocity.benefitscalculator;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BenefitsCalculatorController {
+
+    @RequestMapping("/test")
+    public String string(@RequestParam(value="echo", defaultValue = "Test Works") String echoValue) {
+        return echoValue;
+    }
 }
