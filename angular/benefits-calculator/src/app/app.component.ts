a498c27850e0be20346b042131a970e74aaace5e
ccook <13auction13@gmail.com>
2019-01-12 11:36:26 -0700

    Made changes for inital testing to front end

--- angular/benefits-calculator/src/app/app.component.ts
+++ angular/benefits-calculator/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {EchoService} from "./echo.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'benefits-calculator';
+  title = 'Angular Test Page';
+  echoText = '';
+
+  constructor( private echoService: EchoService) {}
+
+  onClick(input: string) {
+    this.echoService.getEcho(input).subscribe(result =>
+      this.echoText = result.name
+    )
+  }
+
+
 }
--- angular/benefits-calculator/src/app/echo.service.ts
+++ angular/benefits-calculator/src/app/echo.service.ts
@@ -1,20 +1,23 @@
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
+import {Employee} from "./entities/Employee";
 
 @Injectable({
   providedIn: 'root',
 })
 
-class EchoService {
+export class EchoService {
   constructor(
     private http: HttpClient
   ) {}
 
   echoUrl = "localhost:8080/test";
 
-  getEcho(inputString: string): Observable<string> {
+  getEcho(inputString: string): Observable<Employee> {
     const options = {params: new HttpParams().set('echo', inputString)};
-    return this.http.get<string>(this.echoUrl, options)
+    // return this.http.get<string>(this.echoUrl, options)
+    // return this.http.get<string>("https://www.googleapis.com/discovery/v1/apis?fields=")
+    return this.http.get<Employee>("http://localhost:8080/test?echo=Boom")
   }
 }
